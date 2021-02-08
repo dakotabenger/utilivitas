37605707b2e82b1a864a03b0fce5ca3c254c3ff7
Dakota Benger <dakota@bengerlaw.com>
2021-02-08 11:28:19 -0700

    doing this again

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,8 +16,14 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    seederStorage: "sequelize",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };

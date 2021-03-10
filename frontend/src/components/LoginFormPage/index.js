9c022076daadc3742581b0f7f7426cdfb81f8c25
Dakota Benger <dakota@bengerlaw.com>
2021-03-10 11:09:10 -0700

    added demo user button and links

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -27,9 +27,28 @@ function LoginFormPage() {
       });
   };
 
+
+  const handleDemoClick = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential: "demoUser", password:"password" })).then((res) => {
+      dispatch(selectedActions.getUser(res.data.user.id))
+    })
+      .catch((res) => {
+        if (res.data && res.data.errors) setErrors(res.data.errors);
+      });
+  };
+
   return (
     <div className="row ">
-      <div className="col-sm-4"></div>
+      <div className="col-sm-4">
+      <h1 className="site-description">This is a personal social media project where users can be matched based on interests and values, and 
+        then have discussions on feeds. In the future, custom feeds will be added, as well as S3 image hosting, private invite codes, and data verfication (where members of your network can verify things you say on your profile or posts).
+        This project was created by me, Dakota Benger. You can find my <a href="https://www.github.com/dakotabenger">GitHub here</a> and my <a href="https://docs.google.com/document/d/1xkkyMTuGX4L_u2kPIKwc92iVU7KS4pBoDhQofCNMir0/edit">resume here</a> . I am always looking for my next exiciting oppurtunity, so feel free to reach out to me if you have 
+        something in mind. 
+      </h1>
+
+      </div>
       <div className="col-lg-4 row ">
         <div className="col-md-4"></div>
         <div className=" col-md-6 actual-login-container">
@@ -63,9 +82,11 @@ function LoginFormPage() {
         </label>
         </div>
         <div className="row">
-        <div className="col-sm-3"></div>
+        {/* <div className="col-sm-2"></div> */}
         <button className="col-sm-6 login-submit-button" type="submit">Log In</button>
-        <div className="col-sm-3"></div>
+        <button className="col-sm-6 login-submit-button" type="submit" onClick={(e) => handleDemoClick(e)}>Demo Login</button>
+
+        <div className="col-sm-1"></div>
         </div>
       </form>
       </div>
